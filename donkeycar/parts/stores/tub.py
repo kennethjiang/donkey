3c409860c5dfa5c55e0c434bb64e82a953ec99ff
Kenneth Jiang <kenneth.jiang@gmail.com>
2017-09-22 10:18:58 -0700

    Remove dependency on config from tub.py

--- donkeycar/parts/stores/tub.py
+++ donkeycar/parts/stores/tub.py
@@ -16,7 +16,6 @@ from PIL import Image
 
 import numpy as np
 from ... import utils
-from donkeycar.config import load_config
 
 class Tub(object):
     """
@@ -38,8 +37,7 @@ class Tub(object):
 
         self.path = os.path.expanduser(path)
         self.meta_path = os.path.join(self.path, 'meta.json')
-        cfg = load_config()
-        mask_path = os.path.join(cfg.CAR_PATH, 'mask.png')
+        mask_path = os.path.join(self.path, '../../mask.png')
         self.mask = None
         if os.path.isfile(mask_path):
             self.mask = np.array(Image.open(mask_path))
