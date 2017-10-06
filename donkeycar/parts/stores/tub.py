bd24cd2334cbbf6e0e7703d31a6b3557dab3f485
Kenneth Jiang <kenneth.jiang@gmail.com>
2017-10-06 22:39:32 +0000

    new shape

--- donkeycar/parts/stores/tub.py
+++ donkeycar/parts/stores/tub.py
@@ -204,9 +204,7 @@ class Tub(object):
             elif typ == 'image_array':
                 img = Image.open(os.path.join(self.path, val))
                 val = np.array(img)
-                val = undistort(val, balance=0.55)
-                if self.mask is not None:
-                    val = utils.mask_image_array(val, self.mask)
+                val = undistort(val, balance=0.55)[9:79,:,:]
 
             data[key] = val
 
