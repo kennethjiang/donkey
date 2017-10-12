ef7114d8c14c763c6a7ca023117d9773c5daf2ae
Kenneth Jiang <kenneth.jiang@gmail.com>
2017-10-11 18:47:44 -0700

    weighted augmentation

--- donkeycar/parts/stores/augmentation.py
+++ donkeycar/parts/stores/augmentation.py
@@ -1,7 +1,26 @@
 import numpy as np
+import random
+from PIL import Image, ImageDraw
 
 
-def white_unbalance(x, y):
+def identical(data):
+    return data
+
+
+def brightness(data):
+    img_in = data['cam/image_array']
+    image1 = cv2.cvtColor(img_in,cv2.COLOR_RGB2HSV)
+    image1 = np.array(image1, dtype = np.float64)
+    random_bright = .3+np.random.uniform()
+    image1[:,:,2] = image1[:,:,2]*random_bright
+    image1[:,:,2][image1[:,:,2]>255]  = 255
+    image1 = np.array(image1, dtype = np.uint8)
+    data['cam/image_array'] = cv2.cvtColor(image1,cv2.COLOR_HSV2RGB)
+    return data
+
+
+def white_unbalance(data):
+    img_in = data['cam/image_array']
     # Adjust white balance.
     min_channel_high_end = 0.25
     max_channel_low_end = 0.25
@@ -11,8 +30,8 @@ def white_unbalance(x, y):
     rmax = random.random()*max_channel_low_end + 1 - max_channel_low_end
     gmax = random.random()*max_channel_low_end + 1 - max_channel_low_end
     bmax = random.random()*max_channel_low_end + 1 - max_channel_low_end
-    new_image = np.empty((source.height, source.width, 3), dtype=np.float32)
-    image = np.multiply(np.array(source), 1/255.)
+    new_image = np.empty(img_in.shape, dtype=np.float32)
+    image = np.multiply(img_in, 1/255.)
 
     # Make exposure ocasionally brighter
     image = np.clip(np.multiply(image, random.random()*0.3+1.0), 0.0, 1.0)
@@ -20,8 +39,33 @@ def white_unbalance(x, y):
     new_image[:, :, 0] = np.add(np.multiply(image[:, :, 0], (rmax-rmin)), rmin)
     new_image[:, :, 1] = np.add(np.multiply(image[:, :, 1], (gmax-gmin)), gmin)
     new_image[:, :, 2] = np.add(np.multiply(image[:, :, 2], (bmax-bmin)), bmin)
-    new_image = np.multiply(new_image, 255)
-    image = Image.fromarray(np.uint8(new_image))
-    return image
+    data['cam/image_array'] = np.multiply(new_image, 255).astype(np.uint8)
+    return data
+
+def random_rects(data):
+    img_in = data['cam/image_array']
+    # Draw random rectangles over the image so we don't overfit to one feature.
+    w = img_in.shape[1]
+    h = img_in.shape[0]
+    img = Image.fromarray(img_in)
+    draw = ImageDraw.Draw(img)
+    for i in range(random.randint(0, 32)):
+        rs = np.random.rand(4)
+        rc = np.random.randn(3) * 0.3
+        rc = np.clip(rc, -1.0, 1.0) * 127 + 127
+        draw.rectangle([(rs[0]*1.4-0.2)*w, (rs[1]*1.4-0.2)*h, (rs[0]*1.4-0.2)*w + rs[2]*20, (rs[1]*1.4-0.2)*h + rs[3]*20], fill=(int(rc[0]), int(rc[1]), int(rc[2])))
+    data['cam/image_array'] = np.asarray(img)
+    return data
 
 
+WEIGHTED_AUGMENTATIONS = [
+        (identical, 10),
+        (white_unbalance, 8),
+        (random_rects, 8)
+        (brightness, 2)
+        ]
+
+def augment(data):
+    population = [val for val, cnt in WEIGHTED_AUGMENTATIONS for i in range(cnt)]
+    aug = random.choice(population)
+    return aug(data)
